5280d15c0b70b32710097b556808c1910797aa7e
dajjal <turanarican1985@gmail.com>
2024-01-26 20:02:51 +0300

    m

--- main.py
+++ main.py
@@ -1,80 +1,59 @@
-# LIST METHODS
+# LIST COMPREHENSIONS
 
+"""
+List comprehensions provide a concise way to create lists. Common applications are to make new lists where each element is the result of some operations applied to each member of another sequence or iterable, or to create a subsequence of those elements that satisfy a certain condition.
+"""
 
-# list.append(x)
-cars = ["Citroen","Alfa Romeo"]
-cars.append("Mazda")
-print(cars) # ['Citroen', 'Alfa Romeo', 'Mazda']
-cars[len(cars):] = ["Fiat"]
-print(cars) # ['Citroen', 'Alfa Romeo', 'Mazda', 'Fiat']
+squares = []
+for x in range(10):
+    squares.append(x**2)
+print(squares) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+squares_comprehension = [x**2 for x in range(10)]
+print(squares_comprehension) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-# list.extend(x), extend the list with an iterable
-cars.extend(["Honda","Peugeot"])
-print(cars)
-# ['Citroen', 'Alfa Romeo', 'Mazda', 'Fiat', 'Honda', 'Peugeot']
-cars.extend("BMW")
-print(cars) 
-# ['Citroen', 'Alfa Romeo', 'Mazda', 'Fiat', 'Honda', 'Peugeot', 'B', 'M', 'W']
-cars[len(cars):] = "Audi"
-print(cars)
-# ['Citroen', 'Alfa Romeo', 'Mazda', 'Fiat', 'Honda', 'Peugeot', 'B', 'M', 'W', 'A', 'u', 'd', 'i']
+names = ["Halit","Turan","Halide","Tuğba"]
+greets = [f"hello {name}" for name in names]
+for greet in greets:
+    print(greet)
+"""
+hello Halit
+hello Turan
+hello Halide
+hello Tuğba
+"""
 
+# list comprehensions with if
+multiply_if_different = [x*y for x in [2,8,4] for y in [3,2,9] if x != y]
+print(multiply_if_different) # [6, 18, 24, 16, 72, 12, 8, 36]
 
-# list.insert(i,x)
-# insert the item x at the index i
-letters = ["a","b","d","e","f"]
-letters.insert(2,"c")
-print(letters) # ['a', 'b', 'c', 'd', 'e', 'f']
-letters.insert(len(letters),"g")
-print(letters) # ['a', 'b', 'c', 'd', 'e', 'f', 'g'] --- similar to append
+genders = ["male","female"]
+different_halits = [(name,gender) for name in names for gender in [ genders[0] if name == "Halit" or name == "Turan" else "female"]]
+print(different_halits) # [('Halit', 'male'), ('Turan', 'male'), ('Halide', 'female'), ('Tuğba', 'female')]
 
+# Nested list comprehensions
+matrix = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+]
+transpozed = [[row[i] for row in matrix] for i in range(4)]
+print(transpozed)
+#
+# [
+#     [1, 5, 9], 
+#     [2, 6, 10], 
+#     [3, 7, 11], 
+#     [4, 8, 12]
+# ]
+#
 
-# list.remove(x) --- remove the first found x
-# if not found raises ValueError
-cars.remove("Fiat")
-print(cars)
-# ['Citroen', 'Alfa Romeo', 'Mazda', 'Honda', 'Peugeot', 'B', 'M', 'W', 'A', 'u', 'd', 'i']
-
-
-# list.pop([i]) --- i is optional
-# remove the element at the index i (and return it)
-# if i not given remove and return the last element
-saved_before_pop = letters.pop()
-print(saved_before_pop) # g
-print(letters) # ['a', 'b', 'c', 'd', 'e', 'f']
-
-
-# list.clear() --- remove all elements
-cars.clear()
-print(cars) #[]
-
-
-# list.index(x[, start[, end]])
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'c','h', 'i', 'j', 'k', 'c', 'd', 'e', 'l', 'm']
-print(letters.index("c")) # 2
-print(letters.index("c",5)) # 7 ---> search for the argument beginning at index 5
-print(letters.index("c",5,11)) # 7 ---> search for the argument beginning at index 5 and ending at index 11 
-
-
-# list.count(x) 
-print(letters.count("c")) # 3
-
-
-# list.sort() --- sorts in place
-letters.sort()
-print(letters)
-# ['a', 'b', 'c', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
-
-
-# list.reverse() --- reverses in place
-letters.reverse()
-print(letters) 
-# ['m', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'e', 'd', 'd', 'c', 'c', 'c', 'b', 'a']
-
-
-# list.copy() --- return a shallow copy ---> equivalent to [:]
-cars = ["Mazda","Fiat"]
-cars_copy= cars.copy()
-print(cars) # ['Mazda', 'Fiat']
-print(cars_copy) # ['Mazda', 'Fiat']
+# the del statement
+list = [1,2,3,4,5,6,7,8,9]
+del list[1]
+print(list) # [1, 3, 4, 5, 6, 7, 8, 9]
+del list[2:5]
+print(list) # [1, 3, 7, 8, 9]
+del list[:] # does the same as list.clear()
+print(list) # []
+del list # can also be used to delete the entire variable
