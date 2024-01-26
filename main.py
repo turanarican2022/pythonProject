e2a3bc78a2b2ea6c72da9629375842c02132ffb4
dajjal <turanarican1985@gmail.com>
2024-01-26 20:40:27 +0300

    m

--- main.py
+++ main.py
@@ -1,72 +1,26 @@
-# LISTS, TUPLES, SETS
+## DICTIONARIES
 
+cars = {"citroen":2012,"alfa romeo":2008,"mazda":1996,"fiat":2004}
+print(cars["citroen"]) # 2012
 
-# LISTS
+# tuple or list of dictionaries
+cars = (
+    {"brand":"Citroen","year":2012},
+    {"brand":"Alfa Romeo","year":2008},
+    {"brand":"Mazda","year":1996},
+    {"brand":"Fiat","year":2005},
+)
+print(cars[1]["brand"]) # Alfa Romeo
 
-cars = [["Citroen","C3",2012],["Alfa Romeo","156",2008],["Mazda","323",1996]]
-print(cars[0][2]) # 2012
+# turn into dictionary
+cars = [("Citroen",2012),("Mazda",1996)]
+cars_dict = dict(cars)
+print(cars_dict) # {'Citroen': 2012, 'Mazda': 1996}
 
-# append()
-cars.append(["Fiat","Marea",2005])
-print(cars[len(cars)-1][0]) # Fiat
+cars = (("Citroen",2012),("Mazda",1996))
+cars_dict = dict(cars)
+print(cars_dict) # {'Citroen': 2012, 'Mazda': 1996}
 
-#remove()
-cars.remove(cars[0])
-print(cars) # [['Alfa Romeo', '156', 2008], ['Mazda', '323', 1996], ['Fiat', 'Marea', 2005]]
-
-
-# TUPLES
-
-not_a_tuple = "Citroen"
-a_turple = "Citroen",
-print(type(a_turple)) # <class 'tuple'>
-a_not_prefered_tuple_expression = "Citroen","Alfa Romeo"
-print(type(a_not_prefered_tuple_expression)) # <class 'tuple'>
-a_clear_tuple = ("Mazda","Fiat")
-print(type(a_clear_tuple)) # <class 'tuple'>
-
-# append() cannot be used with tuple, instead
-joined_tuple = ("Citroen",) + ("Alfa Romeo",) + a_clear_tuple +("MG",) 
-print(joined_tuple) # ('Citroen', 'Alfa Romeo', 'Mazda', 'Fiat', 'MG')
-
-
-# SETS
-# sets are unordered and contains only unique values
-
-cars = {"Citroen","Alfa Romeo","Mazda","Fiat"}
-print(cars) # {'Fiat', 'Citroen', 'Mazda', 'Alfa Romeo'}
-
-# add to a set
-cars.add("MG")
-print(cars) # {'Citroen', 'MG', 'Mazda', 'Alfa Romeo', 'Fiat'}
-
-# remove from a set
-cars.remove("MG")
-print(cars) # {'Citroen', 'Fiat', 'Mazda', 'Alfa Romeo'}
-
-science_friends = {"Jane","Bob","Rob"}
-art_friends = {"Tina","Jane"}
-
-# difference of sets
-# in a but not in b
-science_but_not_art = science_friends.difference(art_friends)
-print(science_but_not_art) # {'Bob', 'Rob'}
-print(science_friends-art_friends) # {'Bob', 'Rob'}
-
-# symmetric difference (not included in both at the same time)
-# in a or b but not both
-not_in_both = science_friends.symmetric_difference(art_friends)
-print(not_in_both) # {'Bob', 'Rob', 'Tina'}
-print(science_friends^art_friends) # {'Bob', 'Rob', 'Tina'}
-
-# intersection (present in both)
-# in both a and b
-present_in_both = science_friends.intersection(art_friends)
-print(present_in_both) # {'Jane'}
-print(science_friends&art_friends) # {'Jane'}
-
-# union (all in both but without duplication)
-# in a or b or both
-union = science_friends.union(art_friends)
-print(union) # {'Rob', 'Tina', 'Jane', 'Bob'}
-print(science_friends|art_friends) # {'Rob', 'Tina', 'Jane', 'Bob'}
+cars = (["Citroen",2012],["Mazda",1996])
+cars_dict = dict(cars)
+print(cars_dict) # {'Citroen': 2012, 'Mazda': 1996}
