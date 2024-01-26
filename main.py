c14b0c9aa5d3c06c09a3e083e7d473d2630a85d4
dajjal <turanarican1985@gmail.com>
2024-01-26 21:42:39 +0300

    m

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-# WHILE LOOPS
-
-names = []
-ask = True
-while ask:
-    inp=input("please write your name or type \"end\" : ")
-    if inp != "end":
-        names.append(inp)
-    else:
-        ask=False
-print(names)
+# FOR LOOPS
