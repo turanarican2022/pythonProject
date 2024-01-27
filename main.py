b5da5c424def1591b3a04d8c46204191dafe994c
dajjal <turanarican1985@gmail.com>
2024-01-27 21:22:37 +0300

    m

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # FOR LOOPS
+
+for i in range(10):
+    print(i)
+    
+cars = ["Citroen","Alfa Romeo","Mazda","Fiat"]
+for car in cars:
+    print(car)
+# Citroen
+# Alfa Romeo
+# Mazda
+# Fiat
+
+for i in range(len(cars)):
+    print(cars[i]) # the same of above
