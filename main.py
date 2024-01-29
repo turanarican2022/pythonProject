bf5b0a6e724ab51642f7a15dc836869947f4d6e3
dajjal <turanarican1985@gmail.com>
2024-01-30 01:07:54 +0300

    m

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 #####################################
-# THE enumerate() FUNCTION #
+# FUNCTIONS #
 #####################################
 
-# enumerate(iterable, start=0)
-# iterable must be a sequence, an iterator, or some other object which supports iteration.
-# join a number with each element of the iterable
+cars = [{"make":"Citroen","year":2012},{"make":"Alfa Romeo","year":2008}]
 
-cars = ["Citroen","Alfa Romeo","Mazda","Fiat"]
-enumerated = enumerate(cars)
-print(enumerated) # <enumerate object at 0x7f65c414f580>
+def print_car(car):
+    print(f'{car["make"]} was built in {car["year"]}')
+    
+print_car(cars[0]) # Citroen was built in 2012
+print_car(cars[1]) # Alfa Romeo was built in 2008
 
-enumerated = list(enumerated)
-print(enumerated) # [(0, 'Citroen'), (1, 'Alfa Romeo'), (2, 'Mazda'), (3, 'Fiat')]
+# returning functions
 
-enumerated = dict(enumerate(cars,start=2012))
-print(enumerated) # {2012: 'Citroen', 2013: 'Alfa Romeo', 2014: 'Mazda', 2015: 'Fiat'}
+def return_car(make,model):
+    return f"{make} {model}"
 
-for year, car in enumerated.items():
-    print(f"I used a {car} in year {year}")
-#
-# I used a Citroen in year 2012
-# I used a Alfa Romeo in year 2013
-# I used a Mazda in year 2014
-# I used a Fiat in year 2015
-#
+print(return_car("Fiat","Marea")) # Fiat Marea
+
+# named arguments
+# !!! if one of the arguments is named, all the subsquent arguments must be named
+# !!! a named argument must come after an unnamed argument
+
+print(return_car(make="Mazda",model="323")) # Mazda 323
+# print(return_car(make="Citroen","C3")) ---> not allowed
+
+# default parameters
+# !!! if one of the parameters has a default value, all the subsequent parameters must allso have
+# !!! a parameter with a default value must come after parameters without default values
+
+def return_name(name,sur_name="ARICAN"):
+    return f"{name} {sur_name}"
+
+print(return_name("Halit")) # Halit ARICAN
+print(return_name("Halide","Tuğba")) # Halide Tuğba
+print(return_name("Halide",sur_name="Tuğba")) # Halide Tuğba
