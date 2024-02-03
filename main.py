e03bb1391ffecc61ba59383f7b458b2d24f7ea64
dajjal <turanarican1985@gmail.com>
2024-02-03 17:50:08 +0300

    m

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-#####################################
-# FUNCTIONS #
-#####################################
-
-cars = [{"make":"Citroen","year":2012},{"make":"Alfa Romeo","year":2008}]
-
-def print_car(car):
-    print(f'{car["make"]} was built in {car["year"]}')
-    
-print_car(cars[0]) # Citroen was built in 2012
-print_car(cars[1]) # Alfa Romeo was built in 2008
-
-# returning functions
-
-def return_car(make,model):
-    return f"{make} {model}"
-
-print(return_car("Fiat","Marea")) # Fiat Marea
-
-# named arguments
-# !!! if one of the arguments is named, all the subsquent arguments must be named
-# !!! a named argument must come after an unnamed argument
-
-print(return_car(make="Mazda",model="323")) # Mazda 323
-# print(return_car(make="Citroen","C3")) ---> not allowed
-
-# default parameters
-# !!! if one of the parameters has a default value, all the subsequent parameters must allso have
-# !!! a parameter with a default value must come after parameters without default values
-
-def return_name(name,sur_name="ARICAN"):
-    return f"{name} {sur_name}"
-
-print(return_name("Halit")) # Halit ARICAN
-print(return_name("Halide","Tuğba")) # Halide Tuğba
-print(return_name("Halide",sur_name="Tuğba")) # Halide Tuğba
+sentence = "this is a string"
+print(f"{sentence:*>30}") # **************this is a string
+print(f"{sentence:/<30}") # this is a string//////////////
+print(f"{sentence:.^30}") # .......this is a string.......
