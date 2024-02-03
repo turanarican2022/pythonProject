bddf3edbea2524da5bc258962257d054d7964f8a
dajjal <turanarican1985@gmail.com>
2024-02-03 18:03:50 +0300

    m

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-sentence = "this is a string"
-print(f"{sentence:*>30}") # **************this is a string
-print(f"{sentence:/<30}") # this is a string//////////////
-print(f"{sentence:.^30}") # .......this is a string.......
+divide = lambda x,y:x//y
+print(divide(15,3)) # 5
+
+write = lambda car,year: print(f"{car} was made in {year}")
+write("Citroen",2012) # Citroen was made in 2012
+
+write = lambda name="Halit Turan",surname="ARICAN": f"my name is {name} {surname}"
+print(write()) # my name is Halit Turan ARICAN
+print(write(name="Halide",surname="Tuğba")) # my name is Halide Tuğba
+
+def op(num_1,num_2,operand):
+    print(operand(num_1,num_2))
+    
+op(20,2,lambda x,y:x//y) # 10
