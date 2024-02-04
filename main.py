c73bb8087fcfc211b7f79ecf06d6567d322000be
turanarican2022 <turanarican1985@gmail.com>
2024-02-05 00:31:21 +0300

    m

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ class Person:
         self.name = name
         self.cars = cars
     
-    def return_info(self):
-        return f"{self.name} had these cars: {', '.join(self.cars)}"
+    def return_info(self,sex):
+        return f"{self.name} is {sex} and had these cars: {', '.join(self.cars)}"
 
 halide = Person("Halide",["BMW","Audi"])
-print(halide.return_info()) # Halide had these cars: BMW, Audi
+print(halide.return_info("female")) # Halide is female and had these cars: BMW, Audi
 print(halide) # <__main__.Person object at 0x7f9d6b457d00>
 print(halide.__class__) # <class '__main__.Person'>
 print(type(halide)) # <class '__main__.Person'>
