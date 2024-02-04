1050b782d61c9b17187b418e431a6ad8fabb9fd0
turanarican2022 <turanarican1985@gmail.com>
2024-02-05 00:28:17 +0300

    m

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
 #####################################
-# FIRST CLASS FUNCTIONS             #
+# INTRO TO OOP                      #
 #####################################
 
-# functions can be assigned to variables
-
-def greet(name):
-    print(f"hello {name}")
-
-hello = greet
-hello("Tuğba") # hello Tuğba
-
-# mapping functions to variables with dictionaries
-
-functions = {
-    "write_name" : lambda person: f'{person["name"]} is {person["age"]} years old',
-    "write_car": lambda car,car_year: f"Her car is a {car} of {car_year} model year"
-}
-
-person = {
-    "name":"Halide",
-    "age":38,
-
-}
-
-car = {
-    "car":"BMW",
-    "car_year":2024,
-}
-
-def write_info():
-    write_name_here = functions["write_name"]
-    print(write_name_here(person))
-    # Halide is 38 years old
-    print(functions["write_car"](**car))
-    # Her car is a BMW of 2024 model year
+class Person:
+    def __init__(self, name, cars):
+        self.name = name
+        self.cars = cars
     
-write_info()
+    def return_info(self):
+        return f"{self.name} had these cars: {', '.join(self.cars)}"
+
+halide = Person("Halide",["BMW","Audi"])
+print(halide.return_info()) # Halide had these cars: BMW, Audi
+print(halide) # <__main__.Person object at 0x7f9d6b457d00>
+print(halide.__class__) # <class '__main__.Person'>
+print(type(halide)) # <class '__main__.Person'>
