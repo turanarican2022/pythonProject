d67775a39d23986fd9b4c0dc3a44e4dc5210209e
turanarican2022 <turanarican1985@gmail.com>
2024-02-05 21:32:36 +0300

    m

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-# strip() function
-# if no argument given, removes the whitespace
+#############################
+# MAGIC __magic__() METHODS #
+#############################
 
-name = "****Halit Turan ARICAN^^^^"
-print(name.strip("*").strip("^^^^")) #Halit Turan ARICAN
+class Garage:
+    def __init__(self):
+        self.cars = []
+        
+    def __len__(self):
+        return len(self.cars)
+    
+    def __getitem__(self,i):
+        return self.cars[i]
+    
+    def __repr__(self):
+        return f"<class Garage {self.cars}>"
+    
+    def __str__(self):
+        return f"A garage with {len(self.cars)} cars"
+
+garage = Garage()
+garage.cars.append("Citroen")
+garage.cars.append("Mazda")
+
+print(garage.cars)
+
+print(len(garage)) # 2
+
+print(garage[0]) # Citroen
+
+print(garage) # A garage with 2 cars
