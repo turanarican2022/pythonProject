2d915ca36facf3d840569a88c9c5b9e5ec2dc28d
turanarican2022 <turanarican1985@gmail.com>
2024-02-05 18:41:42 +0300

    m

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-#####################################
-# INTRO TO OOP                      #
-#####################################
+# strip() function
+# if no argument given, removes the whitespace
 
-class Person:
-    def __init__(self, name, cars):
-        self.name = name
-        self.cars = cars
-    
-    def return_info(self,sex):
-        return f"{self.name} is {sex} and had these cars: {', '.join(self.cars)}"
-
-halide = Person("Halide",["BMW","Audi"])
-print(halide.return_info("female")) # Halide is female and had these cars: BMW, Audi
-print(halide) # <__main__.Person object at 0x7f9d6b457d00>
-print(halide.__class__) # <class '__main__.Person'>
-print(type(halide)) # <class '__main__.Person'>
+name = "****Halit Turan ARICAN^^^^"
+print(name.strip("*").strip("^^^^")) #Halit Turan ARICAN
