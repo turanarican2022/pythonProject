b3ee7bcbc741f5503698fb3f0267de62329cafc9
turanarican2022 <turanarican1985@gmail.com>
2024-02-06 23:15:39 +0300

    m

--- main.py
+++ main.py
@@ -2,28 +2,62 @@
 # INHERITANCE               #
 #############################
 
-class Electronics:
-    def __init__(self, type, make, model):
-        self.type = type
-        self.make = make
-        self.model = model
+class Person:
     
-    def made_in(self,country):
-        return f"made in {country}"
+    nationality = "Israel"
     
-class Phone(Electronics):
-    def __init__(self, type, make, model, sub_model, year, price):
-        super().__init__(type, make, model)
-        self.sub_model=sub_model
-        self.year=year
-        self.price=price
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
     
-    def built_in(self):
-        return f"built in {self.year}"
+    def person_info(self):
+        return f"{self.name} is {self.age} years old"
     
-huawei_laptop = Electronics("laptop","huawei","d15")
-print(huawei_laptop.made_in("china")) # made in china
+class Teacher(Person):
+    
+    def __init__(self,name,age,job,salary):
+        super().__init__(name,age)
+        self.job = job
+        self.salary=salary
+    
+    def teacher_info(self):
+        return f"{self.name} is a {self.job} and their salary is {self.salary}"
+    
+class Student(Person):
+    
+    def __init__(self, name, age, job, school):
+        super().__init__(name, age)
+        self.job = job
+        self.school=school
+        self.grades = []
+        
+    def student_info(self):
+        return f"{self.name} is a {self.age} year old {self.job} at {self.school}"
+    
+    def add_grade(self,g):
+        self.grades.append(g)
+    
+    def avg_grades(self):
+        return sum(self.grades)/len(self.grades)
+    
 
-huawei_phone = Phone("phone","huawei","nova9","se",2022,8000)
-print(huawei_phone.made_in("china")) # made in china
-print(huawei_phone.built_in()) # built in 2022
+teacher_assida = Teacher("Assida", 29, "teacher", 28000)
+print(teacher_assida.teacher_info())
+# Assida is a teacher and their salary is 28000
+print(teacher_assida.nationality)
+# Israel
+teacher_assida.nationality = "Danish"
+print(teacher_assida.nationality)
+# Danish
+
+student_ethan = Student("Ethan",42,"Student","MIT")
+student_ethan.add_grade(50)
+student_ethan.add_grade(100)
+print(student_ethan.person_info())
+# Ethan is 42 years old
+print(student_ethan.student_info())
+# Ethan is a 42 year old Student at MIT
+print(student_ethan.avg_grades())
+# 75.0
+print(student_ethan.nationality)
+# Israel
