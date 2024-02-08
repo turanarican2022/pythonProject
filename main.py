7ffcf2310361fc578e793c43a371e783493fb36f
turanarican2022 <turanarican1985@gmail.com>
2024-02-08 18:26:35 +0300

    m

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 # @classmethod      #
 #############################
 
-# if a method's (function inside a class) sole job is
-# returning a value @property decorator can be used 
-# and now we don't need parantheses to get that value 
-# when calling that method
-
 class Person:
     
     def __init__(self,name,age):
@@ -20,8 +15,3 @@ class Person:
     def return_info(self):
         return f"{self.name} is {self.age} years old"
         
-
-person=Person("Nitissa",42)
-print(person.give_info) # Nitissa is 42 years old
-print(person.return_info)
-# <bound method Person.return_info of <__main__.Person object at 0x7f247d617d00>>
