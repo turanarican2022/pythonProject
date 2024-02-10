09176dfc5c658be1e718da97059fbdf3a52287e1
turanarican2022 <turanarican1985@gmail.com>
2024-02-11 01:33:18 +0300

    m

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 #######################################
-# modules                             #
+# dictionary comprehension            #
 #######################################
 
-import name_module # name_module
-import car_module as c_mod
+cars = {"Citroen":34000,"Alfa Romeo":28000,"Mazda":6000,"Fiat":18000}
 
-print(__name__) # __main__
+cars_in_eu = {car:price/2 for (car,price) in cars.items()}
 
-print(c_mod.return_car()) # car_module
+print(cars_in_eu)
+# {'Citroen': 17000.0, 'Alfa Romeo': 14000.0, 'Mazda': 3000.0, 'Fiat': 9000.0}
+
+cheap_cars = {car:price for (car,price) in cars.items() if price<20000}
+
+print(cheap_cars)
+# {'Mazda': 6000, 'Fiat': 18000}
+
+cars = {car:("cheap" if price <20000 else "expensive") for (car,price) in cars.items()}
+
+print(cars)
+# {'Citroen': 'expensive', 'Alfa Romeo': 'expensive', 'Mazda': 'cheap', 'Fiat': 'cheap'}
