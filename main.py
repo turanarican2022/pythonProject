7867b69e26fad1240bca1f4f5a964472994f5a88
turanarican2022 <turanarican1985@gmail.com>
2024-02-13 11:01:16 +0300

    m

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ cars = [{"Citroen":"sold"},{"Alfa Romeo":"wrecked"},{"Mazda":"sold"},{"Fiat":"wr
 sold_cars = list(filter(lambda car : "sold" in car.values() ,cars))
 
 print(sold_cars)
+# [{'Citroen': 'sold'}, {'Mazda': 'sold'}]
 
 cars_end = list(map(lambda car: f"{list(car.keys())[0]} is {list(car.values())[0]}" ,cars))
 
 print(cars_end)
+# ['Citroen is sold', 'Alfa Romeo is wrecked', 'Mazda is sold', 'Fiat is wrecked and sold']
