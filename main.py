996c7f2d59a49b654f5639bad95c4836c94951cc
turanarican2022 <turanarican1985@gmail.com>
2024-02-13 11:04:25 +0300

    m

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ cars_end = list(map(lambda car: f"{list(car.keys())[0]} is {list(car.values())[0
 
 print(cars_end)
 # ['Citroen is sold', 'Alfa Romeo is wrecked', 'Mazda is sold', 'Fiat is wrecked and sold']
+
+doubles_or_thrices = list(map(lambda x: x*2 if x%2 == 0 else x*3,range(10)))
+
+print(doubles_or_thrices)
+# [0, 3, 4, 9, 8, 15, 12, 21, 16, 27]
