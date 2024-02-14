a511ee8222df6bb204e7e6d2330d337e54e01ab5
turanarican2022 <turanarican1985@gmail.com>
2024-02-14 22:41:41 +0300

    m

--- main.py
+++ main.py
@@ -3,18 +3,16 @@
 ########################### to Ethan...
 
 class Student:
-    def __init__(self,name,age,school):
-        self.name = name
+    def __init__(self,name,age):
+        self.name=name
         self.age = age
-        self.school = school
-        
-    def __str__(self):
-        return f"{self.name} is {self.age} years old and a succesful student at {self.school}"
     
-    @classmethod
-    def from_dashed_string(cls,dashed_str):
-        name,age,school = dashed_str.split("-")
-        cls(name,age,school)
-        
-print(Student("Ethan",19,"MIT"))
-# Ethan is 19 years old and a succesful student at MIT
+    def __str__(self):
+        return f"{self.name} is {self.age} years old."
+    
+    @staticmethod
+    def school_age(age):
+        return f"a kid must be at least {age} years old to begin school"
+    
+print(Student.school_age(7))
+# a kid must be at least 7 years old to begin school
