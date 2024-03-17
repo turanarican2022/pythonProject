4d36c067b8f450a774395b8932ae82b15fdd325a
turanarican2022 <turanarican1985@gmail.com>
2024-03-17 20:08:36 +0300

    m

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-#######################################
-# raising error and isinstance()      #
-########################### to Ethan...
+####################################################
+# safely accessing dictionary elements with .get() #
+######################################## to Ethan...
 
-class Student:
-    
-    _students = []
-    
-    def __init__(self,name,age):
-        if age < 7:
-            raise ValueError("This kid is too young to be a student.")
-        self.name = name
-        self.age = age
-        Student._add_student(name,age)
-    
-    @classmethod
-    def _add_student(cls,name,age):
-        cls._students.append({"name":name,"age":age})
-        
-    @classmethod
-    def students_info(cls):
-        return [f'{student["name"]} is {student["age"]} years old' for student in cls._students]
-    
-    def __str__(self):
-        return f"{self.name} is {self.age} years old."
+a_dict = {"key1":"value1", "key3":"value3"}
 
-    
-ethan = Student("Ethan",19)
-print(isinstance(ethan,Student)) # True
-halit = Student("Halit",14)
-print(Student.students_info()) # ['Ethan is 19 years old', 'Halit is 14 years old']
-turan = Student("Turan",6) # ValueError: This kid is too young to be a student.
+# print(a_dict["key2"]) # KeyError: 'key2'
+
+print(a_dict.get("key1")) # value1
+print(a_dict.get("key2")) # None
+
+
+
 
